c514cf132d0659170cde3e2a03bdb23f901aa8af
phil <phild@protonmail.com>
2022-04-27 20:26:38 +0000

    returning iterations, added number of constraints check

--- src/least_squares/src/GaussNewton.hpp
+++ src/least_squares/src/GaussNewton.hpp
@@ -32,23 +32,29 @@ namespace pd::vslam::least_squares{
         r->stepSize = Eigen::VectorXd::Zero(_maxIterations);
         r->x = Eigen::MatrixXd::Zero(_maxIterations,nParameters);
         r->cov.reserve(_maxIterations);
-        size_t i = 0;
-        for(; i < _maxIterations; i++ )
+        for(size_t i = 0; i < _maxIterations; i++ )
         {
             TIMED_SCOPE(timerI,"solve ( " + std::to_string(i) + " )");
+
             // We want to solve dx = (JWJ)^(-1)*JWr
             // This can be solved with cholesky decomposition (Ax = b)
             // Where A = (JWJ + lambda * I), x = dx, b = JWr
             auto ne = problem->computeNormalEquations();
 
             const double det = ne->A.determinant();
-            if(!std::isfinite(det) || det < 1e-6){
-                SOLVER( WARNING ) << i << " > " << "STOP. Bad Hessian.";
+            if(ne->nConstraints < nParameters)
+            {
+                SOLVER( WARNING ) << i << " > " << "STOP. Not enough constraints: " << ne->nConstraints << " / " << nParameters;
                 break;
             }
+            if(!std::isfinite(det) || std::abs(det) < 1e-6){
+                SOLVER( WARNING ) << i << " > " << "STOP. Bad Hessian. det| H | = " << det;
+                break;
+            }
+            
             SOLVER(DEBUG) << i << " > A=\n" << ne->A << "\nb=\n" << ne->b.transpose() << "\nnConstraints="<< ne->nConstraints << " chi2=" <<ne->chi2;
 
-            r->chi2(i) = ne->chi2 / ne->nConstraints;
+            r->chi2(i) = ne->chi2;
 
             const double dChi2 = i > 0 ? r->chi2(i)- r->chi2(i-1) : 0;
             if (i > 0 && dChi2 > 0 )
@@ -57,11 +63,12 @@ namespace pd::vslam::least_squares{
                 problem->setX(r->x.row(i-1));
                 break;
             }
-            const auto dx = ne->A.ldlt().solve(ne->b);
+            const VecXd dx = ne->A.ldlt().solve(ne->b);
             problem->updateX(dx);
             r->x.row(i) = problem->x();
             r->stepSize(i) = dx.norm();
             r->cov.push_back(ne->A.inverse());
+            r->iteration = i;
 
             SOLVER( INFO ) << "Iteration: " << i << " chi2: " << r->chi2(i) << " dChi2: " << dChi2 << " stepSize: " << r->stepSize(i) 
             << " Points: " << ne->nConstraints << "\nx: " << problem->x().transpose() << "\ndx: " << dx.transpose();
@@ -77,7 +84,7 @@ namespace pd::vslam::least_squares{
             }
 
         }
-        LOG_PLT("SolverGN") << std::make_shared<vis::PlotGaussNewton>(i,r->chi2,r->stepSize);
+        LOG_PLT("SolverGN") << std::make_shared<vis::PlotGaussNewton>(r->iteration,r->chi2,r->stepSize);
         return r;
     }
 
